44ab2a4b8654b60068e5d72e49097f0f05cd3c48
veeru18 <85793587+veeru18@users.noreply.github.com>
2022-12-27 15:06:50 +0530

    Update and rename VariableValExt_.java to VariableValExt.java
    updated VariableValExtractor file for decimal values too

--- VariableValExt.java
+++ VariableValExt.java
@@ -10,7 +10,7 @@ public class VariableValExt {
 
 	public static void valInsert(List<String> senten,Set<Character> oper) {
 		List<String> variables=new ArrayList<>();
-		List<Integer> values=new ArrayList<>();
+		List<Double> values=new ArrayList<>();
 		for(String s:senten) {
 			int indx=s.indexOf(':');
 //			System.out.println(indx);
@@ -31,7 +31,7 @@ public class VariableValExt {
 						variables.add(var);
 						continue;
 					}
-					if(!oper.contains(varOpers.charAt(i)))
+					if(!oper.contains(varOpers.charAt(i))||varOpers.charAt(i)=='.')
 						var+=varOpers.charAt(i);
 					else {
 						variables.add(var);
@@ -46,29 +46,33 @@ public class VariableValExt {
 				for(int i=0;i<valueString.length();i++) {
 					if(valueString.charAt(i)=='('||valueString.charAt(i)==')') continue;
 					if(valueString.charAt(i)=='=') {
-						values.add(Integer.valueOf(val));
+						values.add(Double.valueOf(val));
 						val="";
 						continue;
 					}
 					if(i==valueString.length()-1) {
 						val+=valueString.charAt(i);
-						values.add(Integer.valueOf(val));
+						values.add(Double.valueOf(val));
 						continue;
 					}
-					if(!oper.contains(valueString.charAt(i)))
+					if(!oper.contains(valueString.charAt(i))||valueString.charAt(i)=='.')
 						val+=valueString.charAt(i);
 					else {
-						values.add(Integer.valueOf(val));
+						values.add(Double.valueOf(val));
 						val="";
 					}
 				}
 			}
 		}
 //		System.out.println(values+"\n"+variables);
-		
-		for(int i=0;i<values.size();i++) {
-			System.out.println(variables.get(i)+":"+values.get(i));
-		}
+		if(variables.size()==values.size()) {
+			for(int i=0;i<values.size();i++) {
+				double res=values.get(i);
+				if(res-(int)res>0)
+				System.out.println(variables.get(i)+":"+values.get(i));
+				else System.out.println(variables.get(i)+":"+(long) res);
+			}
+		}else System.out.println("Error unequal values and variables");
 	}
 	public static void main(String[] args) {
 //		String t=new String(" sin jn s=-s").replaceAll(" ", "");
